1de1ff73e76d1298c0c5dbdde96695dc95502526
lennono <owenlennon3@gmail.com>
2019-08-28 02:31:57 +0100

    Proper multiprocessing

--- barberProblem.py
+++ barberProblem.py
@@ -2,25 +2,18 @@ import threading
 import time
 import random
 from multiprocessing import Process, Queue, cpu_count
-# Not a fair solution will need to use a queue
-# Stupid python
-#barberReady = threading.Semaphore(value=3)
-#customerReady = threading.Semaphore(value=0)
-#numberOfFreeSeats = 10 # Atomic variable
+
 
 def barber(queue):
     while True:
         queue.get()
-        #customerReady.acquire()
         print("Barber ")
-        #numberOfFreeSeats += 1
-        #barberReady.release()
         time.sleep(random.randint(2, 17)) # Hair cut time
 
 def customer(queue):
     queue.put('Work')
-
     print("customer")
+    # Can put up a while loop here to mimic a continuous 
         
 
 
